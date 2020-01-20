866cbce4653c397782c78209ded228fc3f570afc
Max Zheng <34936931+54bp6cl6@users.noreply.github.com>
2020-01-20 10:06:07 +0800

    Add files via upload

---
+++ Server/ServerForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Server
+{
+	partial class ServerForm
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.log_TB = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.online_LB = new System.Windows.Forms.ListBox();
+			this.button = new System.Windows.Forms.Button();
+			this.my_port_TB = new System.Windows.Forms.TextBox();
+			this.my_IP_TB = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// log_TB
+			// 
+			this.log_TB.Location = new System.Drawing.Point(252, 11);
+			this.log_TB.Margin = new System.Windows.Forms.Padding(2);
+			this.log_TB.Multiline = true;
+			this.log_TB.Name = "log_TB";
+			this.log_TB.ReadOnly = true;
+			this.log_TB.Size = new System.Drawing.Size(314, 436);
+			this.log_TB.TabIndex = 35;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label3.Location = new System.Drawing.Point(77, 170);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(96, 26);
+			this.label3.TabIndex = 34;
+			this.label3.Text = "線上列表";
+			// 
+			// online_LB
+			// 
+			this.online_LB.FormattingEnabled = true;
+			this.online_LB.ItemHeight = 12;
+			this.online_LB.Location = new System.Drawing.Point(15, 203);
+			this.online_LB.Name = "online_LB";
+			this.online_LB.Size = new System.Drawing.Size(226, 244);
+			this.online_LB.TabIndex = 33;
+			// 
+			// button
+			// 
+			this.button.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.button.Location = new System.Drawing.Point(54, 96);
+			this.button.Name = "button";
+			this.button.Size = new System.Drawing.Size(140, 58);
+			this.button.TabIndex = 32;
+			this.button.Text = "建立伺服器";
+			this.button.UseVisualStyleBackColor = true;
+			this.button.Click += new System.EventHandler(this.button_Click);
+			// 
+			// my_port_TB
+			// 
+			this.my_port_TB.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.my_port_TB.Location = new System.Drawing.Point(95, 44);
+			this.my_port_TB.Name = "my_port_TB";
+			this.my_port_TB.Size = new System.Drawing.Size(146, 23);
+			this.my_port_TB.TabIndex = 31;
+			this.my_port_TB.Text = "6666";
+			// 
+			// my_IP_TB
+			// 
+			this.my_IP_TB.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.my_IP_TB.Location = new System.Drawing.Point(95, 11);
+			this.my_IP_TB.Name = "my_IP_TB";
+			this.my_IP_TB.ReadOnly = true;
+			this.my_IP_TB.Size = new System.Drawing.Size(146, 23);
+			this.my_IP_TB.TabIndex = 30;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label2.Location = new System.Drawing.Point(11, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(73, 20);
+			this.label2.TabIndex = 29;
+			this.label2.Text = "我的Port";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label1.Location = new System.Drawing.Point(11, 14);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(56, 20);
+			this.label1.TabIndex = 28;
+			this.label1.Text = "我的IP";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(587, 460);
+			this.Controls.Add(this.log_TB);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.online_LB);
+			this.Controls.Add(this.button);
+			this.Controls.Add(this.my_port_TB);
+			this.Controls.Add(this.my_IP_TB);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Server_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox log_TB;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ListBox online_LB;
+		private System.Windows.Forms.Button button;
+		private System.Windows.Forms.TextBox my_port_TB;
+		private System.Windows.Forms.TextBox my_IP_TB;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Server/ServerForm.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+	public partial class ServerForm : Form
+	{
+		bool opened;
+		TcpListener tcpListener;
+		Socket client;
+		Thread th_serv, th_clie;
+		Dictionary<string, Socket> users;
+		Dictionary<Socket, Socket> games;
+
+		public ServerForm()
+		{
+			InitializeComponent();
+
+			my_IP_TB.Text = GetMyIP();
+			opened = false;
+			users = new Dictionary<string, Socket>();
+			games = new Dictionary<Socket, Socket>();
+		}
+
+		private string GetMyIP()
+		{
+			IPAddress[] ip = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+			foreach (IPAddress it in ip)
+				if (it.AddressFamily == AddressFamily.InterNetwork) return it.ToString();
+			return "";
+		}
+
+		private delegate void LogCallback(string log);
+		private void Log(string log)
+		{
+			if (this.InvokeRequired)
+			{
+				LogCallback logCallback = new LogCallback(Log);
+				this.Invoke(logCallback, log);
+			}
+			else
+			{
+				if (log_TB.Text != "") log_TB.Text += Environment.NewLine;
+				log_TB.Text += log;
+				log_TB.ScrollBars = ScrollBars.Vertical;
+				log_TB.SelectionStart = log_TB.Text.Length;
+				log_TB.ScrollToCaret();
+			}
+		}
+
+		private delegate void UpdateListCallback();
+
+		private void button_Click(object sender, EventArgs e)
+		{
+			if (!opened)
+			{
+				if (int.TryParse(my_port_TB.Text, out int port) && port >= 1024 && port <= 65535)
+				{
+					opened = true;
+					button.Text = "關閉伺服器";
+					th_serv = new Thread(ServerSub);
+					th_serv.IsBackground = true;
+					th_serv.Start();
+				}
+				else
+				{
+					Log("Port設置錯誤");
+				}
+			}
+			else
+			{
+				opened = false;
+				tcpListener.Stop();
+				th_serv.Abort();
+				foreach (Socket socket in users.Values)
+				{
+					socket.Close();
+				}
+				users.Clear();
+				UpdateList();
+				Log("成功關閉伺服器");
+				button.Text = "建立伺服器";
+			}
+		}
+
+		private void UpdateList()
+		{
+			if (this.InvokeRequired)
+			{
+				UpdateListCallback updateListCallback = new UpdateListCallback(UpdateList);
+				this.Invoke(updateListCallback);
+			}
+			else
+			{
+				online_LB.Items.Clear();
+				foreach (string str in users.Keys)
+				{
+					online_LB.Items.Add(str);
+				}
+			}
+		}
+
+		private void Server_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			opened = false;
+			try
+			{
+				tcpListener.Stop();
+				th_serv.Abort();
+			}
+			catch { }
+		}
+
+		private void ServerSub()
+		{
+			tcpListener = new TcpListener(new IPEndPoint(IPAddress.Parse(GetMyIP()), int.Parse(my_port_TB.Text)));
+			tcpListener.Start(10);
+			Log("成功建立伺服器");
+			while (opened)
+			{
+				try
+				{
+					client = tcpListener.AcceptSocket();
+					th_clie = new Thread(Listen);
+					th_clie.IsBackground = true;
+					th_clie.Start();
+				}
+				catch { }
+			}
+		}
+
+		private void Send(Socket socket, string msg)
+		{
+			socket.Send(Encoding.Default.GetBytes(msg + "`"));
+			Log("<發送>" + msg);
+		}
+
+		private void Broadcast(string msg)
+		{
+			foreach (Socket socket in users.Values)
+			{
+				Send(socket, msg);
+			}
+		}
+
+		private void Listen()
+		{
+			EndPoint ep = (EndPoint)client.RemoteEndPoint;
+			Socket socket = client;
+			byte[] data = new byte[1024];
+
+			try
+			{
+				while (opened)
+				{
+					data = new byte[1024];
+					int len = socket.ReceiveFrom(data, ref ep);
+					string command = Encoding.Default.GetString(data).Trim();
+					Log("<收到>" + command);
+					string[] element = command.Split('`');
+					if (element[0] == "checknick")
+					{
+						if (!users.ContainsKey(element[1]))
+						{
+							string temp = "join`";
+							foreach (string s in users.Keys)
+							{
+								temp += s + "`";
+							}
+							Send(socket, temp.Substring(0, temp.Length - 1));
+							Broadcast("new`" + element[1]);
+							users.Add(element[1], socket);
+							UpdateList();
+						}
+						else
+						{
+							string temp = "nick`";
+							foreach (string s in users.Keys)
+							{
+								temp += s + "`";
+							}
+							Send(socket, temp.Substring(0, temp.Length - 1));
+						}
+					}
+					else if (element[0] == "out")
+					{
+						users.Remove(element[1]);
+						UpdateList();
+						Broadcast("out`" + element[1]);
+						Log(element[1] + " 離開伺服器");
+						if (games.ContainsKey(socket))
+						{
+							Send(games[socket], "run");
+							games.Remove(games[socket]);
+							games.Remove(socket);
+						}
+					}
+					else if (element[0] == "invite")
+					{
+						if (!games.ContainsKey(users[element[1]]))
+						{
+							Send(users[element[1]], "invite`" + element[2]);
+						}
+						else
+						{
+							Send(socket, "playing`" + element[1]);
+						}
+					}
+					else if (element[0] == "accept")
+					{
+						Send(users[element[1]], "accept`" + element[2] + "`" + element[3]);
+						games.Add(users[element[1]], users[element[2]]);
+						games.Add(users[element[2]], users[element[1]]);
+					}
+					else if (element[0] == "reject")
+					{
+						Send(users[element[1]], "reject");
+					}
+					else if (element[0] == "start")
+					{
+						Send(socket, "start`" + element[1] + "`" + element[2]);
+						Send(games[socket], "start`" + element[2] + "`" + element[1]);
+					}
+					else if (element[0] == "end")
+					{
+						if (games.ContainsKey(socket))
+						{
+							Send(games[socket], "run");
+							games.Remove(games[socket]);
+							games.Remove(socket);
+						}
+					}
+					else
+					{
+						string sendstr = command.Substring(0, command.Length - 1);
+						Send(socket, sendstr);
+						Send(games[socket], sendstr);
+					}
+				}
+			}
+			catch (Exception ex) { }
+		}
+	}
+}
